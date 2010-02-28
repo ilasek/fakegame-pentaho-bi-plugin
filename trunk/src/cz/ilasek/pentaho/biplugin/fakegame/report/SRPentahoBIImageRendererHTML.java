08e605bdfd753cffb0a453ea937c1fdac9f0faab
ivo.lasek <ivo.lasek@186debb0-1434-11df-9d2a-2365f6df44e6>
2010-02-28 09:44:18 +0000

    Plugin is basically ready in beta version.

---
+++ trunk/src/cz/ilasek/pentaho/biplugin/fakegame/report/SRPentahoBIImageRendererHTML.java
@@ -0,0 +1,40 @@
+package cz.ilasek.pentaho.biplugin.fakegame.report;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import game.report.SRHTMLRenderer;
+import game.report.srobjects.ISRObjectRenderer;
+import game.report.srobjects.SRImage;
+
+public class SRPentahoBIImageRendererHTML implements ISRObjectRenderer 
+{
+    protected final String IMAGE_RESOLVER = "/pentaho/getImage?image=";
+    protected SRImage image;
+    protected SRHTMLRenderer srhtmlRenderer;
+    protected final String reportImagesDir;
+
+    public SRPentahoBIImageRendererHTML(SRImage image, SRHTMLRenderer srhtmlRenderer, String reportImagesDir) {
+        this.image = image;
+        this.srhtmlRenderer = srhtmlRenderer;
+        this.reportImagesDir = reportImagesDir;
+    }
+
+    public void render() {
+        srhtmlRenderer.append("<P><DIV class='image'><IMG src = '");
+        srhtmlRenderer.append(getImagePath());
+        srhtmlRenderer.append("'/><DIV>" + image.getCaption() + "</DIV></DIV></P>\n");
+    }
+    
+    protected String getImagePath()
+    {
+        String imagePath;
+        try {
+            imagePath = IMAGE_RESOLVER + URLEncoder.encode("/" + reportImagesDir + "/" + image.getImageFile(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            imagePath = IMAGE_RESOLVER + "/" + reportImagesDir + "/" + image.getImageFile();
+        }        
+        
+        return imagePath;
+    }
+}
